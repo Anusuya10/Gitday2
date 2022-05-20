4a98641739f954ecded7588d082bf07cf9c6c6c1
Anusuya10 <anubbytaj2510@gmail.com>
2022-05-20 18:59:20 +0530

    new cod

--- src/org/com/Desktop.java
+++ src/org/com/Desktop.java
@@ -3,6 +3,7 @@ package org.com;
 public class Desktop extends Computer {
 	private void desktopSize() {
 		 	System.out.println("Dharu");
+		 	System.out.println("Dharu");
 
 	}
 	public static void main(String[] args) {
